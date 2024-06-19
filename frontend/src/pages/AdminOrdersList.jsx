aae8082c612b3cc0d6a04c9d777ffb01824e9f32
Nischit1 <49693663+shresthanischit18@users.noreply.github.com>
2024-06-19 13:52:07 +0545

    first commit

--- frontend/src/pages/AdminOrdersList.jsx
+++ frontend/src/pages/AdminOrdersList.jsx
@@ -1,24 +1,24 @@
 import {
-    Alert,
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Dialog,
-    DialogBody,
-    DialogHeader,
-    Typography,
-} from '@material-tailwind/react';
-import { useQuery } from '@tanstack/react-query';
-import { format } from 'date-fns';
-import { useState } from 'react';
-import { v4 as uuid } from 'uuid';
-import { Loader } from '../components';
-import { getRequest } from '../utils/apiHandler';
+  Alert,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Dialog,
+  DialogBody,
+  DialogHeader,
+  Typography,
+} from "@material-tailwind/react";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import { Loader } from "../components";
+import { getRequest } from "../utils/apiHandler";
 
 const AdminOrderList = () => {
   const { data: ordersList, isLoading } = useQuery({
-    queryKey: ['Admin Order List'],
+    queryKey: ["Admin Order List"],
     queryFn: async () => {
       const res = await getRequest({
         endpoint: `/orders/`,
@@ -29,13 +29,13 @@ const AdminOrderList = () => {
   });
 
   const TABLE_HEAD = [
-    'SN',
-    'User',
-    'Email',
-    'Phone Number',
-    'Order Placed Date',
-    'Order Total',
-    'Action',
+    "SN",
+    "User",
+    "Email",
+    "Phone Number",
+    "Order Placed Date",
+    "Order Total",
+    "Action",
   ];
 
   const TABLE_ROWS = ordersList || [];
@@ -44,10 +44,10 @@ const AdminOrderList = () => {
     <>
       {isLoading && <Loader />}
       <Card>
-        <CardHeader floated={false} shadow={false} className='rounded-none'>
-          <div className='mb-8 flex items-center justify-between gap-8'>
+        <CardHeader floated={false} shadow={false} className="rounded-none">
+          <div className="mb-8 flex items-center justify-between gap-8">
             {/* Table title */}
-            <Typography variant='h5' color='blue-gray'>
+            <Typography variant="h5" color="blue-gray">
               Orders List
             </Typography>
           </div>
@@ -55,8 +55,8 @@ const AdminOrderList = () => {
 
         {TABLE_ROWS.length == 0 && (
           <CardBody>
-            <Alert variant='ghost' color='blue'>
-              {' '}
+            <Alert variant="ghost" color="blue">
+              {" "}
               No orders found
             </Alert>
           </CardBody>
@@ -64,18 +64,20 @@ const AdminOrderList = () => {
 
         {TABLE_ROWS.length !== 0 && (
           <>
-            <CardBody className='overflow-scroll px-0'>
-              <table className='w-full table-auto text-left'>
+            <CardBody className="overflow-scroll px-0">
+              <table className="w-full table-auto text-left">
                 <thead>
                   <tr>
-                    {TABLE_HEAD.map(head => (
+                    {TABLE_HEAD.map((head) => (
                       <th
                         key={uuid()}
-                        className='border-y border-blue-gray-100 bg-blue-gray-50/50 p-4'>
+                        className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                      >
                         <Typography
-                          variant='small'
-                          color='blue-gray'
-                          className='font-normal leading-none opacity-70'>
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
                           {head}
                         </Typography>
                       </th>
@@ -83,71 +85,77 @@ const AdminOrderList = () => {
                   </tr>
                 </thead>
 
-                <tbody className='text-sm'>
+                <tbody className="text-sm">
                   {TABLE_ROWS.map((order, index) => {
                     const { _id, createdAt, user, orderItems, orderTotal } =
                       order;
 
                     const isLast = index === TABLE_ROWS.length - 1;
                     const classes = isLast
-                      ? 'p-4'
-                      : 'p-4 border-b border-blue-gray-50';
+                      ? "p-4"
+                      : "p-4 border-b border-blue-gray-50";
 
                     return (
                       <tr key={uuid()}>
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
                             {index + 1}
                           </Typography>
                         </td>
 
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
-                            {user.fullName}
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user?.fullName}
                           </Typography>
                         </td>
 
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
-                            {user.email}
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user?.email}
                           </Typography>
                         </td>
 
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
-                            {user.phoneNumber}
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user?.phoneNumber}
                           </Typography>
                         </td>
 
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
                             {format(
                               new Date(createdAt),
-                              'dd MMMM, yyyy HH:mm:ss'
+                              "dd MMMM, yyyy HH:mm:ss"
                             )}
                           </Typography>
                         </td>
 
                         <td className={classes}>
                           <Typography
-                            variant='small'
-                            color='blue-gray'
-                            className='font-normal'>
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
                             {orderTotal} Reward Points
                           </Typography>
                         </td>
@@ -178,7 +186,7 @@ const AdminOrderItemsList = ({ orderItems = [] }) => {
 
   return (
     <div>
-      <Button variant='filled' color='green' size='sm' onClick={handleOpen}>
+      <Button variant="filled" color="green" size="sm" onClick={handleOpen}>
         View Orders Items
       </Button>
 
@@ -186,34 +194,36 @@ const AdminOrderItemsList = ({ orderItems = [] }) => {
         <DialogHeader>Order Items List</DialogHeader>
 
         <DialogBody>
-          <div className='overflow-x-auto'>
-            <table className='table-auto min-w-full divide-y divide-gray-200'>
-              <thead className='bg-gray-50'>
+          <div className="overflow-x-auto">
+            <table className="table-auto min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
                   <th
-                    scope='col'
-                    className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
                     Title
                   </th>
                   <th
-                    scope='col'
-                    className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
                     Item Point
                   </th>
                 </tr>
               </thead>
-              <tbody className='bg-white divide-y divide-gray-200'>
+              <tbody className="bg-white divide-y divide-gray-200">
                 {/* JavaScript map function to iterate over order items */}
                 {/* 'item' represents each individual order item in the array */}
                 {/* You can adjust the keys based on your MongoDB schema */}
                 {/* For example, item.title, item.point, etc. */}
                 {orderItems.map((item, index) => (
                   <tr key={index}>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>{item.title}</div>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.title}</div>
                     </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>{item.point}</div>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.point}</div>
                     </td>
                   </tr>
                 ))}
@@ -226,4 +236,3 @@ const AdminOrderItemsList = ({ orderItems = [] }) => {
     </div>
   );
 };
-
